dd82db12f22b40b5ef1dcd144324bda54f59f7bd
DPil22 <86372766+DPil22@users.noreply.github.com>
2022-10-04 21:09:22 +0200

    Little update, removed executable

--- c/first.c
+++ c/first.c
@@ -2,17 +2,23 @@
 
 int main() {
   int N;
-  int x = 7;
-  printf("Number input=");
+  int x;
+  printf("Number N input=");
   scanf("%d", &N);
+  printf("Number x input=");
+  scanf("%d", &x);
   
-  printf("Your number is %d\n", N);
+  printf("Your number is %d, which is %s\n", N, isEven(N) ? "even" : "odd");
   
-  printf("You %s divide %d with %d\n", (canDivide(N, x) ? "can" : "can''t"), N, x);
+  printf("You %s divide %d with %d\n", canDivide(N, x) ? "can" : "can''t", N, x);
   
   return 0;
 }
 
 int canDivide(int N, int x) {
   return N % x == 0;
+}
+
+int isEven(int N) {
+  return N % 2 == 0;
 }
